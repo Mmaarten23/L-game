79a47fba12e294e1efd7317e48eb0e4f544638a8
Maarten Magits <mmaartendisc@gmail.com>
2020-02-01 12:12:16 +0100

    initial layout

--- code.js
+++ code.js
@@ -10,13 +10,13 @@ function generate_board_html(board){
           row_html += `<td>  </td>`;
         }
         else if (board[i][j] == 1) {
-          row_html += `<td>  </td>`;
+          row_html += `<td> 1 </td>`;
         }
         else if (board[i][j] == 2) {
-          row_html += `<td>  </td>`;
+          row_html += `<td> 2 </td>`;
         }
         else if (board[i][j] == 3) {
-          row_html += `<td>  </td>`;
+          row_html += `<td> 3 </td>`;
         }
         else if (board[i][j] == 4) {
           row_html += `<td>  </td>`;
@@ -33,20 +33,28 @@ function generate_board_html(board){
 }
 
 
-function empty_board(){
-  board = [[0,0,0,0],
-           [0,0,0,0],
-           [0,0,0,0],
-           [0,0,0,0]]
-  return board
-}
-
 function drawBoard(board,container){
   document.getElementById(container).innerHTML = generate_board_html(board);
-  
 }
 
 function main(){
-  board = empty_board()
+  board = initialise_board()
   drawBoard(board, "board")
+}
+
+function initialise_board(){
+  /*
+  3 = NEUTRAL
+  1 = PLAYER_1
+  */
+  board = [[3,1,1,0],
+           [0,2,1,0],
+           [0,2,1,0],
+           [0,2,2,3]]
+  return board
+}
+
+
+window.onload = function(){
+  main()
 }
